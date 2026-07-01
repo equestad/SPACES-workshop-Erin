6e82976ae970f28eb2341ca9ef84a478cb0b2e88
Erin Questad <ejquestad@cpp.edu>
2026-07-01 13:36:55 -0700

    added color for year

--- scripts/script1.R
+++ scripts/script1.R
@@ -5,3 +5,7 @@ library(ratdat)
 
 ggplot(complete_old, aes(x=hindfoot_length, y = weight))+
   geom_point()
+
+#adding color for year
+ggplot(complete_old, aes(x=hindfoot_length, y = weight, color=year))+
+  geom_point()
